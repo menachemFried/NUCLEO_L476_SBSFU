5c83a35094a48483b1fb9624ce4ee367110ecf00
mfrid <mfried@binata.com>
2025-07-07 16:01:06 +0300

    accept changes

--- Project/SECoreBin/Scripts/SECBOOT_AES128_GCM_AES128_GCM_AES128_GCM.template.sh
+++ Project/SECoreBin/Scripts/SECBOOT_AES128_GCM_AES128_GCM_AES128_GCM.template.sh
@@ -1,160 +1,253 @@
-#!/bin/bash -
-#Post build for SECBOOT_AES128_GCM_WITH_AES128_GCM
-# arg1 is the build directory
-# arg2 is the elf file path+name
-# arg3 is the bin file path+name
-# arg4 is the firmware Id (1/2/3)
-# arg5 is the version
-# arg6 when present forces "bigelf" generation
-projectdir=$1
-FileName=${3##*/}
-execname=${FileName%.*}
-elf=$2
-bin=$3
-fwid=$4
-version=$5
+# ==============================================================================
+#
+# ORIGINAL STMICROELECTRONICS SCRIPT - FOR REFERENCE ONLY
+#
+# This file contains the original script, broken down into functional parts.
+# Each part is commented out and will be replaced step-by-step with
+# the new, refactored implementation.
+#
+# ==============================================================================
 
-SecureEngine=${0%/*}
+# ==============================================================================
+# Part A: Parameter & Variable Definition
+# ==============================================================================
+#A# #!/bin/bash -
+#A# #Post build for SECBOOT_AES128_GCM_WITH_AES128_GCM
+#A# # arg1 is the build directory
+#A# # arg2 is the elf file path+name
+#A# # arg3 is the bin file path+name
+#A# # arg4 is the firmware Id (1/2/3)
+#A# # arg5 is the version
+#A# # arg6 when present forces "bigelf" generation
+#A# projectdir=$1
+#A# FileName=${3##*/}
+#A# execname=${FileName%.*}
+#A# elf=$2
+#A# bin=$3
+#A# fwid=$4
+#A# version=$5
+#A# 
+#A# SecureEngine=${0%/*}
+#A# 
+#A# userAppBinary=$projectdir"/../Binary"
+#A# 
+#A# sfu=$userAppBinary"/"$execname".sfu"
+#A# sfb=$userAppBinary"/"$execname".sfb"
+#A# sign=$userAppBinary"/"%execname".sign"
+#A# headerbin=$userAppBinary"/"$execname"sfuh.bin"
+#A# bigbinary=$userAppBinary"/SBSFU_"$execname".bin"
+#A# elfbackup=$userAppBinary"/SBSFU_"$execname".elf"
+#A#  
+#A# nonce=$SecureEngine"/../Binary/nonce.bin"
+#A# magic="SFU"$fwid
+#A# oemkey=$SecureEngine"/../Binary/OEM_KEY_COMPANY"$fwid"_key_AES_GCM.bin"
+#A# partialbin=$userAppBinary"/Partial"$execname".bin"
+#A# partialsfb=$userAppBinary"/Partial"$execname".sfb"
+#A# partialsfu=$userAppBinary"/Partial"$execname".sfu"
+#A# partialsign=$userAppBinary"/Partial"$execname".sign"
+#A# partialoffset=$userAppBinary"/Partial"$execname".offset"
+#A# ref_userapp=$projectdir"/RefUserApp.bin"
+#A# offset=512
+#A# alignment=16
 
-userAppBinary=$projectdir"/../Binary"
+# ==============================================================================
+# Part B: Complex Path Calculation
+# ==============================================================================
+#B# 
+#B# current_directory=`pwd`
+#B# cd "$SecureEngine/../../"
+#B# SecureDir=`pwd`
+#B# cd "$current_directory"
+#B# sbsfuelf="$SecureDir/2_Images_SBSFU/STM32CubeIDE/Debug/SBSFU.elf"
+#B# current_directory=`pwd`
+#B# cd "$1/../../../../../../Middlewares/ST/STM32_Secure_Engine/Utilities/KeysAndImages"
+#B# basedir=`pwd`
+#B# cd "$current_directory"
 
-sfu=$userAppBinary"/"$execname".sfu"
-sfb=$userAppBinary"/"$execname".sfb"
-sign=$userAppBinary"/"%execname".sign"
-headerbin=$userAppBinary"/"$execname"sfuh.bin"
-bigbinary=$userAppBinary"/SBSFU_"$execname".bin"
-elfbackup=$userAppBinary"/SBSFU_"$execname".elf"
+# ==============================================================================
+# Part C: Toolchain Selection
+# ==============================================================================
+#C# # test if window executable usable
+#C# prepareimage=$basedir"/win/prepareimage/prepareimage.exe"
+#C# uname | grep -i -e windows -e mingw >/dev/null > /dev/null 2>&1
+#C# if [ $? -eq 0 ] && [  -e "$prepareimage" ]; then
+#C#   echo "prepareimage with windows executable"
+#C#   PATH=$basedir"\\win\\prepareimage":$PATH > /dev/null 2>&1
+#C#   cmd=""
+#C#   prepareimage="prepareimage.exe"
+#C# else
+#C#   # line for python
+#C#   echo "prepareimage with python script"
+#C#   prepareimage=$basedir/prepareimage.py
+#C#   cmd="python"
+#C# fi
 
-nonce=$SecureEngine"/../Binary/nonce.bin"
-magic="SFU"$fwid
-oemkey=$SecureEngine"/../Binary/OEM_KEY_COMPANY"$fwid"_key_AES_GCM.bin"
-partialbin=$userAppBinary"/Partial"$execname".bin"
-partialsfb=$userAppBinary"/Partial"$execname".sfb"
-partialsfu=$userAppBinary"/Partial"$execname".sfu"
-partialsign=$userAppBinary"/Partial"$execname".sign"
-partialoffset=$userAppBinary"/Partial"$execname".offset"
-ref_userapp=$projectdir"/RefUserApp.bin"
-offset=512
-alignment=16
+# ==============================================================================
+# Part D: Create Output Directory
+# ==============================================================================
+#D# 
+#D# # Make sure we have a Binary sub-folder in UserApp folder
+#D# if [ ! -e $userAppBinary ]; then
+#D# mkdir $userAppBinary
+#D# fi
 
-current_directory=`pwd`
-cd "$SecureEngine/../../"
-SecureDir=`pwd`
-cd "$current_directory"
-sbsfuelf="$SecureDir/2_Images_SBSFU/STM32CubeIDE/Debug/SBSFU.elf"
-current_directory=`pwd`
-cd "$1/../../../../../../Middlewares/ST/STM32_Secure_Engine/Utilities/KeysAndImages"
-basedir=`pwd`
-cd "$current_directory"
-# test if window executable usable
-prepareimage=$basedir"/win/prepareimage/prepareimage.exe"
-uname | grep -i -e windows -e mingw >/dev/null > /dev/null 2>&1
-if [ $? -eq 0 ] && [  -e "$prepareimage" ]; then
-  echo "prepareimage with windows executable"
-  PATH=$basedir"\\win\\prepareimage":$PATH > /dev/null 2>&1
-  cmd=""
-  prepareimage="prepareimage.exe"
-else
-  # line for python
-  echo "prepareimage with python script"
-  prepareimage=$basedir/prepareimage.py
-  cmd="python"
-fi
+# ==============================================================================
+# Part E.1: Main Packaging - Encrypt
+# ==============================================================================
+#E.1# 
+#E.1# command=$cmd" "$prepareimage" enc -k "$oemkey" -n "$nonce" "$bin" "$sfu
+#E.1# $command > $projectdir"/output.txt"
+#E.1# ret=$?
 
-# Make sure we have a Binary sub-folder in UserApp folder
-if [ ! -e $userAppBinary ]; then
-mkdir $userAppBinary
-fi
+# ==============================================================================
+# Part E.2: Main Packaging - Sign
+# ==============================================================================
+#E.2# if [ $ret -eq 0 ]; then
+#E.2#   command=$cmd" "$prepareimage" sign -k "$oemkey" -n "$nonce" "$bin" "$sign
+#E.2#   $command >> $projectdir"/output.txt"
+#E.2#   ret=$?
 
-command=$cmd" "$prepareimage" enc -k "$oemkey" -n "$nonce" "$bin" "$sfu
-$command > $projectdir"/output.txt"
-ret=$?
-if [ $ret -eq 0 ]; then
-  command=$cmd" "$prepareimage" sign -k "$oemkey" -n "$nonce" "$bin" "$sign
-  $command >> $projectdir"/output.txt"
-  ret=$?
-  if [ $ret -eq 0 ]; then
-    command=$cmd" "$prepareimage" pack -m "$magic" -k "$oemkey"  -r 112 -v "$version" -n "$nonce" -f "$sfu" -t "$sign" "$sfb" -o "$offset
-    $command >> $projectdir"/output.txt"
-    ret=$?
-    if [ $ret -eq 0 ]; then
-      command=$cmd" "$prepareimage" header -m "$magic" -k  "$oemkey" -r 112 -v "$version"  -n "$nonce" -f "$sfu" -t "$sign" -o "$offset" "$headerbin
-      $command >> $projectdir"/output.txt"
-      ret=$?
-      if [ $ret -eq 0 ]; then
-        command=$cmd" "$prepareimage" merge -v 0 -e 1 -i "$headerbin" -s "$sbsfuelf" -u "$elf" "$bigbinary
-        $command >> $projectdir"/output.txt"
-        ret=$?
-        #Partial image generation if reference userapp exists
-        if [ $ret -eq 0 ] && [ -e "$ref_userapp" ]; then
-          echo "Generating the partial image .sfb"
-          echo "Generating the partial image .sfb" >> $projectdir"/output.txt"
-          command=$cmd" "$prepareimage" diff -1 "$ref_userapp" -2 "$bin" "$partialbin" -a "$alignment" --poffset "$partialoffset
-          $command >> $projectdir"/output.txt"
-          ret=$?
-          if [ $ret -eq 0 ]; then
-            command=$cmd" "$prepareimage" enc -k "$oemkey" -i "$nonce" "$partialbin" "$partialsfu
-            $command >> $projectdir"/output.txt"
-            ret=$?
-            if [ $ret -eq 0 ]; then
-              command=$cmd" "$prepareimage" sign -k "$oemkey" -n "$nonce" "$partialbin" "$partialsign
-              $command >> $projectdir"/output.txt"
-              ret=$?
-              if [ $ret -eq 0 ]; then
-                command=$cmd" "$prepareimage" pack -m "$magic" -k "$oemkey" -r 112 -v "$version" -i "$nonce" -f "$sfu" -t "$sign" -o "$offset" --pfw "$partialsfu" --ptag "$partialsign" --poffset  "$partialoffset" "$partialsfb
-                $command >> $projectdir"/output.txt"
-                ret=$?
-              fi
-            fi
-          fi
-        fi
-        if [ $ret -eq 0 ] && [ $# = 6 ]; then
-          echo "Generating the global elf file SBSFU and userApp"
-          echo "Generating the global elf file SBSFU and userApp" >> $projectdir"/output.txt"
-          uname | grep -i -e windows -e mingw > /dev/null 2>&1
-          if [ $? -eq 0 ]; then
-            # Set to the default installation path of the Cube Programmer tool
-            # If you installed it in another location, please update PATH.
-            PATH="C:\\Program Files (x86)\\STMicroelectronics\\STM32Cube\\STM32CubeProgrammer\\bin":$PATH > /dev/null 2>&1
-            programmertool="STM32_Programmer_CLI.exe"
-          else
-            which STM32_Programmer_CLI > /dev/null
-            if [ $? = 0 ]; then
-              programmertool="STM32_Programmer_CLI"
-            else
-              echo "fix access path to STM32_Programmer_CLI"
-            fi
-          fi
-          command=$programmertool" -ms "$elf" "$headerbin" "$sbsfuelf
-          $command >> $projectdir"/output.txt"
-          ret=$?
-        fi
-      fi
-    fi
-  fi
-fi
+# ==============================================================================
+# Part E.3: Main Packaging - Pack
+# ==============================================================================
+#E.3#   if [ $ret -eq 0 ]; then 
+#E.3#     command=$cmd" "$prepareimage" pack -m "$magic" -k "$oemkey"  -r 112 -v "$version" -n "$nonce" -f "$sfu" -t "$sign" "$sfb" -o "$offset
+#E.3#     $command >> $projectdir"/output.txt"
+#E.3#     ret=$?
 
+# ==============================================================================
+# Part E.4: Main Packaging - Header
+# ==============================================================================
+#E.4#     if [ $ret -eq 0 ]; then
+#E.4#       command=$cmd" "$prepareimage" header -m "$magic" -k  "$oemkey" -r 112 -v "$version"  -n "$nonce" -f "$sfu" -t "$sign" -o "$offset" "$headerbin
+#E.4#       $command >> $projectdir"/output.txt"
+#E.4#       ret=$?
 
-if [ $ret -eq 0 ]; then
-  rm $sign
-  rm $sfu
-  rm $headerbin
-  if [ -e "$ref_userapp" ]; then
-    rm $partialbin
-    rm $partialsfu
-    rm $partialsign
-    rm $partialoffset
-  fi
-  exit 0
-else
-  echo "$command : failed" >> $projectdir"/output.txt"
-  if [ -e  "$elf" ]; then
-    rm  $elf
-  fi
-  if [ -e "$elfbackup" ]; then
-    rm  $elfbackup
-  fi
-  echo $command : failed
-  read -n 1 -s
-  exit 1
-fi
+# ==============================================================================
+# Part E.5: Main Packaging - Merge
+# ==============================================================================
+#E.5#       if [ $ret -eq 0 ]; then
+#E.5#         command=$cmd" "$prepareimage" merge -v 0 -e 1 -i "$headerbin" -s "$sbsfuelf" -u "$elf" "$bigbinary
+#E.5#         $command >> $projectdir"/output.txt"
+#E.5#         ret=$?
+
+# ==============================================================================
+# Part F.1: Partial Image (Optional) - Diff
+# ==============================================================================
+#F.1#         #Partial image generation if reference userapp exists
+#F.1#         if [ $ret -eq 0 ] && [ -e "$ref_userapp" ]; then
+#F.1#           echo "Generating the partial image .sfb"
+#F.1#           echo "Generating the partial image .sfb" >> $projectdir"/output.txt"
+#F.1#           command=$cmd" "$prepareimage" diff -1 "$ref_userapp" -2 "$bin" "$partialbin" -a "$alignment" --poffset "$partialoffset
+#F.1#           $command >> $projectdir"/output.txt"
+#F.1#           ret=$?
+
+# ==============================================================================
+# Part F.2: Partial Image (Optional) - Encrypt
+# ==============================================================================
+#F.2#           if [ $ret -eq 0 ]; then
+#F.2#             command=$cmd" "$prepareimage" enc -k "$oemkey" -i "$nonce" "$partialbin" "$partialsfu
+#F.2#             $command >> $projectdir"/output.txt"
+#F.2#             ret=$?
+
+# ==============================================================================
+# Part F.3: Partial Image (Optional) - Sign
+# ==============================================================================
+#F.3#             if [ $ret -eq 0 ]; then
+#F.3#               command=$cmd" "$prepareimage" sign -k "$oemkey" -n "$nonce" "$partialbin" "$partialsign
+#F.3#               $command >> $projectdir"/output.txt"
+#F.3#               ret=$?
+
+# ==============================================================================
+# Part F.4: Partial Image (Optional) - Pack
+# ==============================================================================
+#F.4#               if [ $ret -eq 0 ]; then
+#F.4#                 command=$cmd" "$prepareimage" pack -m "$magic" -k "$oemkey" -r 112 -v "$version" -i "$nonce" -f "$sfu" -t "$sign" -o "$offset" --pfw "$partialsfu" --ptag "$partialsign" --poffset  "$partialoffset" "$partialsfb
+#F.4#                 $command >> $projectdir"/output.txt"
+#F.4#                 ret=$?
+#F.4#               fi
+#F.3#             fi
+#F.2#           fi
+#F.1#         fi
+
+# ==============================================================================
+# Part G: Merged ELF Generation (Optional)
+# ==============================================================================
+#G#         if [ $ret -eq 0 ] && [ $# = 6 ]; then
+#G#           echo "Generating the global elf file SBSFU and userApp"
+#G#           echo "Generating the global elf file SBSFU and userApp" >> $projectdir"/output.txt"
+#G#           uname | grep -i -e windows -e mingw > /dev/null 2>&1
+#G#           if [ $? -eq 0 ]; then
+#G#             # Set to the default installation path of the Cube Programmer tool
+#G#             # If you installed it in another location, please update PATH.
+#G#             PATH="C:\\Program Files (x86)\\STMicroelectronics\\STM32Cube\\STM32CubeProgrammer\\bin":$PATH > /dev/null 2>&1
+#G#             programmertool="STM32_Programmer_CLI.exe"
+#G#           else
+#G#             which STM32_Programmer_CLI > /dev/null
+#G#             if [ $? = 0 ]; then
+#G#               programmertool="STM32_Programmer_CLI"
+#G#             else
+#G#               echo "fix access path to STM32_Programmer_CLI"
+#G#             fi
+#G#           fi
+#G#           command=$programmertool" -ms "$elf" "$headerbin" "$sbsfuelf
+#G#           $command >> $projectdir"/output.txt"
+#G#           ret=$?
+#G#         fi
+#E.5#       fi
+#E.4#     fi
+#E.3#   fi
+#E.2# fi
+
+# ==============================================================================
+#
+# NEW SCRIPT IMPLEMENTATION
+#
+# ==============================================================================
+
+#!/bin/sh
+
+# --- Configuration ---
+set -e
+set -u
+
+# --- Global Variables ---
+# To be populated later
+LOG_INFO_MODE=1
+LOG_DEBUG_MODE=1
+LOG_ERROR_MODE=1
+LOG_INFO_FILE=""
+LOG_DEBUG_FILE=""
+LOG_ERROR_FILE=""
+
+# --- Function Definitions ---
+
+# Print usage information and exit
+usage() {
+    # To be implemented
+    echo "Usage: ..."
+}
+
+# Log a message to the configured output
+_log() {
+    # To be implemented
+    echo "$@"
+}
+
+info_log()  { _log "$1" "INFO"  "$2"; }
+debug_log() { _log "$1" "DEBUG" "$2"; }
+error_log() { _log "$1" "ERROR" "$2"; }
+
+error_exit() {
+    error_log "$1" "$2"
+    exit 1
+}
+
+# --- Main Script Logic ---
+main() {
+    # To be implemented
+    info_log "$LINENO" "Script starting..."
+}
+
+# --- Execute Main Function ---
+main "$@"
